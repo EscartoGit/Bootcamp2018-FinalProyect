9d18fa35e7b072710a1865e0ed096a11b53f42b9
Escarto <wdm.mdq90@gmail.com>
2018-03-08 16:14:41 -0300

    Creation of principal components to start from the same base

--- video-reproductor/src/components/App.js
+++ video-reproductor/src/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './../logo.svg';
 import './../css/App.css';
+import Description from './Descriptor.js';
+import Reproductor from './Reproductor.js';
+import SearchBar from './SearchBar.js'
+import VideoList from './VideoList.js'
 
 class App extends Component {
   render() {
@@ -8,7 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Classes Added</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
---
+++ video-reproductor/src/components/Descriptor.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+class Descriptor extends Component{
+
+}
