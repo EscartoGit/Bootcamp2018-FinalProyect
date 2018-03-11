b6f2fad5f5c929b7989a3b75386de3fbe4d4c5c0
Leandro Bisceglie <leam.90@gmail.com>
2018-03-11 17:28:37 -0300

    search bar

--- video-reproductor/src/components/App.js
+++ video-reproductor/src/components/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './../logo.svg';
+import React, {Component} from 'react';
 import './../css/App.css';
 import Description from './Descriptor.js';
 import Reproductor from './Reproductor.js';
@@ -10,13 +9,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <SearchBar  />
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Classes Added</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
