a690f39801ee3b701a74370f13d7c287346ee16e
MaxiPawlowski <maxipawloski@gmail.com>
2018-03-12 23:45:10 -0300

    VideoList

--- video-reproductor/src/components/App.js
+++ video-reproductor/src/components/App.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react';
-import logo from './../logo.svg';
+import React, {Component} from 'react';
 import './../css/App.css';
 import Description from './Descriptor.js';
 import Reproductor from './Reproductor.js';
 import SearchBar from './SearchBar.js'
 import VideoList from './VideoList.js'
 
+import YoutubeApi from './YoutubeApi.js'
+import Debounce from './Debounce.js'
+
+const API_KEY = "AIzaSyAGDLCl4PnihPeLdNzB-GxjpAHpyM2rqGY";
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: null }
+
+    this.handleSearch = Debounce(this.handleSearch.bind(this), 1000)
+  }
+
+  handleSearch(searchTerm){
+    YoutubeApi({key: API_KEY, searchTerm})
+      .then((videos) => {
+          this.setState({videos})
+      })
+      .catch(function (reason) {
+          console.error(reason);
+      });
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+      <SearchBar Search={this.handleSearch} />
+      {console.log(this.state)}
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Classes Added</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
--- video-reproductor/src/components/SearchBar.js
+++ video-reproductor/src/components/SearchBar.js
@@ -1,5 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from "react";
+import './../css/SearchBar.css';
 
-class SearchBar extends Component{
+import logo from './../logo.png';
 
+
+
+
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.setSearchTerm = this.setSearchTerm.bind(this)
+
+    }
+    setSearchTerm(searchTerm) {
+    
+        this.setState({ searchTerm })
+    
+        if (searchTerm != '') {
+          this.props.Search(searchTerm)
+        }
+      }
+
+    render() {
+        return (
+            <div className="search-bar">
+                <div className="topnav">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <div className="search-container">
+                        <form>
+                            <input
+                                type="text"
+                                onChange={e => {
+                                this.setSearchTerm(e.target.value.trim())
+                            }}
+                                placeholder="Search.."
+                                name="search"/>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
 }
+
+export default SearchBar;
